86389c8d4e4579d0daeae9cc279527b92404a384
AjithKrishnaAithal <ajith.aithal20890@gmail.com>
2017-08-10 13:51:14 +0530

    format timestamps using momentjs'

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,10 @@
+var moment = require('moment');
+
 var generateMessage = (from,text)=>{
   return {
   from,
   text,
-  createdat:new Date().getTime()
+  createdat:moment().valueOf()
 };
             };
 
@@ -10,7 +12,7 @@ var generateLocationMessage=(from,latitude,longitude)=>{
   return{
     from,
     url:`http://www.google.com/maps?q=${latitude},${longitude}`,
-    createdat:new Date().getTime()
+    createdat:moment().valueOf()
   };
 };
 
